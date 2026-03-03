bd3ae6cb55380947f6ec129b3cff2efbfb630910
Sai pradeep <142535919+PradeepSaiC@users.noreply.github.com>
2026-03-03 13:51:21 +0530

    Initialize React application with main components

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import NavBar from "./components/NavBar";
+import Body from "./components/Body";
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
