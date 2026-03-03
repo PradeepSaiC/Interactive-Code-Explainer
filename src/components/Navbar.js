a06f2170932fd2deaf0f92a4a9d713e21632d903
Sai pradeep <142535919+PradeepSaiC@users.noreply.github.com>
2026-03-03 13:52:44 +0530

    Add Navbar component for interactive code explainer

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+
+const NavBar = () => {
+  return (
+    <div>
+                <div className="bg-gray-800 shadow-2xl h-16 text-zinc-100 flex items-center">
+            <h2 className="p-2 text-2xl ml-2">Interative Code Explainer (Beta)</h2>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar;
