0489669d6a3b7bb94c1de1c72bcd1c19a7ccf942
Sai pradeep <142535919+PradeepSaiC@users.noreply.github.com>
2026-03-31 14:54:00 +0530

    Change NavBar background color to dark shade

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@
 const NavBar = () => {
   return (
     <div>
-                <div className="bg-gray-800 shadow-2xl h-16 text-zinc-100 flex items-center">
+                <div className="bg-[#09090B] shadow-2xl h-16 text-zinc-100 flex items-center">
             <h2 className="p-2 text-2xl ml-2">Interative Code Explainer (Beta)</h2>
         </div>
     </div>
