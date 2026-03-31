f32649bced5103136731673c3f7864c051917bd1
Sai pradeep <142535919+PradeepSaiC@users.noreply.github.com>
2026-03-31 15:21:49 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ const NavBar = () => {
     <div>
                 <div className="bg-[#09090B] shadow-2xl h-16 text-zinc-100 flex items-center">
             <h2 className="p-2 text-2xl ml-2 text-gray-100 font-semibold text-lg leading-tight tracking-tight">Interative Code Explainer (Beta)</h2>
+                        <p className="text-xs text-gray-400 font-medium">Break & Understand what every code block is doing</p>
         </div>
     </div>
   )
